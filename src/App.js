080c39f3cb6184267e40c1710de7374a5e1760e7
Samat Kalshabekov <skalshabekov@beeline.kz>
2022-09-19 14:49:44 +0600

    forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const faculties = [
+  {
+    key: 1,
+    name: "ENG",
+  },
+  {
+    key: 2,
+    name: "LNG",
+  },
+  {
+    key: 3,
+    name: "FYL",
+  },
+  {
+    key: 4,
+    name: "JUR",
+  },
+  {
+    key: 5,
+    name: "MED",
+  },
+];
+export default function App() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isChecked, setIsChecked] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  const handleChangeFirstName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleChangeLastName = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleChangeFaculty = (index) => {
+    const newChecked = isChecked.map((ch, idx) => {
+      if (index === idx) return !ch;
+      else return ch;
+    });
+
+    setIsChecked(newChecked);
+  };
+
+  const handleSubmitForm = (event, data) => {
+    event.preventDefault();
+
+    console.log("data", data);
+    // long hard proces
+    setTimeout(() => {
+      console.log({ name: name, lastName: lastName, faculties: isChecked });
+    }, 10000);
+    console.log("DOne!");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* input first Name */}
+      <form>
+        <p>First name</p>
+        <input
+          name="fname"
+          type="text"
+          placeholder="Enter your name"
+          onChange={handleChangeFirstName}
+          value={name}
+        />
+        {/* input last Name */}
+        <p>Last name</p>
+        <input
+          name="lname"
+          type="text"
+          placeholder="Enter your Last name"
+          onChange={handleChangeLastName}
+          value={lastName}
+        />
+
+        {/* input checkbox */}
+
+        <p>Faculty</p>
+
+        {faculties.map((faculty, index) => (
+          <div key={faculty.key}>
+            <span>{faculty.name}</span>
+            <input
+              name="faculty"
+              type="checkbox"
+              checked={isChecked[index]}
+              onChange={() => handleChangeFaculty(index)}
+            />
+          </div>
+        ))}
+
+        <button type="submit" onClick={handleSubmitForm}>
+          Submit form
+        </button>
+      </form>
     </div>
   );
 }
 
-export default App;
+// cd ..
+// npx create-react-app form-app
